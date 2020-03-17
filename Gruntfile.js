b8ffebae44066526ad800d1790657e28e6792df1
wujinya <2601519403@qq.com>
2020-03-17 21:28:07 +0800

    合并  压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,63 @@ module.exports = function (grunt) {
         csslintrc: './.csslintrc'   
       },
       src: ['*.css']      
+    },
+    concat: {
+      js: {
+        src: ['baidu.js'],
+        dest: 'dist/bundle.js'                      
+      },
+      css:{
+        src:['*.css'],
+        dest:'dist/bundle.css'                               
+      }
+    },
+    sprite: {
+      all: {
+        src: '*.jpg',
+        dest: 'dist/spritesheet.png',
+        destCss: 'dist/sprites.css'                                            
+      }
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false                       
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'                 
+      }     
+    },
+    cssmin: {  
+      'dist/baidu.min.css': 'baidu.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/baidu.min.js': 'baidu.js'                      
+        }             
+      }                  
+    },
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['./*.{png,jpg,gif}'],
+        dest: 'dist/'                                        
+      }
     }
 
   });
    
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-htmlhint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.registerTask('default', ['less:compile','htmlhint','csslint']);
+  //grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['less:compile','htmlhint','concat:js','concat:css','sprite','htmlmin','cssmin','uglify:release','imagemin']);
 
 };
