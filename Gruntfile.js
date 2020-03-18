27ebd9f5dbc69feaca04b9d2ee7fa5763b826dda
wujinya <2601519403@qq.com>
2020-03-18 08:16:19 +0800

    modify .travis.yml

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,6 @@ module.exports = function (grunt) {
    
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-htmlhint');
-  //grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-spritesmith');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
